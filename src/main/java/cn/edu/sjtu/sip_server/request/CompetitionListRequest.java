9658c9ba70267660fd427a04b70da24110375562
Qinyue Wu <141250149@smail.nju.edu.cn>
2019-06-25 12:26:08 +0800

    竞赛request
    创建竞赛request

---
+++ src/main/java/cn/edu/sjtu/sip_server/request/CompetitionListRequest.java
@@ -0,0 +1,51 @@
+package com.amateur.sip.server.request;
+
+public class CompetitionListRequest {
+    /**
+     * 页数
+     */
+    public int page;
+    /**
+     * 发布时间
+     */
+    public String releaseTime;
+    /**
+     * 组织者
+     */
+    public String host;
+    /**
+     * 竞赛类型
+     */
+    public String type;
+    /**
+     * 竞赛状态
+     */
+    public String status;
+    /**
+     * 排序方式
+     */
+    public String orderBy;
+    /**
+     * 竞赛级别
+     */
+    public String level;
+    /**
+     * 竞赛名
+     */
+    public String name;
+
+    public CompetitionListRequest(int page, String releaseTime, String host, String type, String status, String orderBy, String level, String name) {
+        this.page = page;
+        this.releaseTime = releaseTime;
+        this.host = host;
+        this.type = type;
+        this.status = status;
+        this.orderBy = orderBy;
+        this.level = level;
+        this.name = name;
+    }
+
+    public CompetitionListRequest() {
+
+    }
+}
