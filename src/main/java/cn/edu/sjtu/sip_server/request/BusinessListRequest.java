f78d1880b5565cfc9270ea7e7c870080e335a04b
Qinyue Wu <141250149@smail.nju.edu.cn>
2019-06-25 12:24:26 +0800

    项目列表request
    项目列表request

---
+++ src/main/java/cn/edu/sjtu/sip_server/request/BusinessListRequest.java
@@ -0,0 +1,52 @@
+package com.amateur.sip.server.request;
+
+public class BusinessListRequest {
+    /**
+     * 页数
+     */
+    public int page;
+    /**
+     * 类型
+     */
+    public String type;
+    /**
+     * 类别
+     */
+    public String category;
+    /**
+     * 状态
+     */
+    public String status;
+    /**
+     * 来源
+     */
+    public String source;
+    /**
+     * 学生类型
+     */
+    public String studentType;
+    /**
+     * 排序方式
+     */
+    public String orderBy;
+    /**
+     * 名字
+     */
+    public String name;
+
+
+    public BusinessListRequest() {
+
+    }
+
+    public BusinessListRequest(int page, String type, String category, String status, String source, String studentType, String orderBy, String name) {
+        this.page = page;
+        this.type = type;
+        this.category = category;
+        this.status = status;
+        this.source = source;
+        this.studentType = studentType;
+        this.orderBy = orderBy;
+        this.name = name;
+    }
+}
